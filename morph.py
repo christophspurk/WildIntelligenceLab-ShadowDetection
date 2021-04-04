bbc1708479961004fbc1d6a9a914855f7426033a
kkoppe <katrin.koppe@rwth-aachen.de>
2021-04-04 16:46:24 +0200

    update classify function

--- morph.py
+++ morph.py
@@ -308,19 +308,29 @@ def get_shadows(
 
     return bin_cuts, rgb_cuts, df_cnt
 
+    """
+    Returns:
+    updated dataframe with added classification labels and confidence scores
+    """
 def classify_shadows(rgb_shadows, df_shadows, weights_path):
-    # load trained model with weights
-    os.chdir(weights_path)
+    # load pretrained model with weights
+    # os.chdir(weights_path)
     model = tf.keras.applications.DenseNet121(include_top=True, classes=3, pooling=None)
     model.load_weights(weights_path)
     # predict rgb_cuts with loaded model
-
     classes = []
     confidence_scores = []
+    predicted_label = []
     predictions = model.predict(rgb_shadows)
-    score = tf.nn.softmax(predictions[0])
-    # class + confidence in liste speichern
-    # results als neue colums dem df hinzufuegen
+    for p in predictions:
+        predicted_label.append(np.argmax(predictions[p]))
+
+    # score = tf.nn.softmax(predictions[0])
+    # classes.append(class_names[np.argmax(score)])
+    # confidence_scores.append(round(100 * np.max(score), 3))
+    df_shadows['class'] = classes
+    df_shadows['confidence_score'] = confidence_scores
+
     return df_shadows
 
 if __name__ == "__main__":
@@ -335,3 +345,4 @@ if __name__ == "__main__":
     print(df_morph)
     plt.imshow(rgb_cuts[0])
     plt.show()
+    # classify_shadows(rgb_cuts, df_morph, pretrained_model_path)
