3c70fcc653653da6803a8afbad46aa68079560d0
Miriam Koppe <mkoppe1@smail.uni-koeln.de>
2021-04-04 20:09:03 +0200

    added visualization

---
+++ visualization.py
@@ -0,0 +1,35 @@
+import os
+import cv2
+import skimage
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from skimage.color import label2rgb
+from skimage.measure import label
+
+def classification_visualization(dataframe, bin_mask_shadow, rgb_image):
+    # get pic and convert
+    rgb_img = cv2.imread(rgb_image)
+    rgb_img = cv2.cvtColor(rgb_img, cv2.COLOR_BGR2RGB)
+
+    bin_img = cv2.imread(bin_mask_shadow)
+
+    # label the shadows
+    bin_img_gray = label(bin_img)
+    image_label_overlay = label2rgb(bin_img_gray, rgb_img, bg_label = 0, kind='overlay')
+    
+    # TODO: CHECK end pic if its still blue
+    image_label_overlay[bin_img == 0]= 0
+    image_label_overlay[bin_img == 0]= rgb_img[bin_img == 0]/255
+    #rgb_img = cv2.cvtColor(rgb_img, cv2.COLOR_BGR2RGB)
+
+    z = dataframe
+
+    for i, rows in z.iterrows():
+        z.iloc[i]["x_mask"]
+        rgb_img = cv2.rectangle(image_label_overlay, (z.iloc[i]["x_mask"], z.iloc[i]["y_mask"]), (z.iloc[i]["x_mask"]+z.iloc[i]["w_bb"], z.iloc[i]["y_mask"]+z.iloc[i]["h_bb"]), (255,0,0), 5)
+        cv2.putText(image_label_overlay, str(z.iloc[i]["l_length"]), (z.iloc[i]["x_mask"], z.iloc[i]["y_mask"]-20), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255,0,0),2)
+        center_coordinates = (int(z.iloc[i]["x_mask"]+ 0.5 * z.iloc[i]["w_bb"]), int(z.iloc[i]["y_mask"]+ 0.5 * z.iloc[i]["h_bb"]))
+        cv2.circle(image_label_overlay, center_coordinates, 10, (255, 0, 0), 10)
+    
+    return rgb_image
