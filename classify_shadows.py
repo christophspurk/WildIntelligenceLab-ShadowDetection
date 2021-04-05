60050d188aa6ec87f9f5ef5ece4b6ec8140c2d27
kkoppe <katrin.koppe@rwth-aachen.de>
2021-04-05 10:37:47 +0200

    minor changes

--- classify_shadows.py
+++ classify_shadows.py
@@ -24,7 +24,7 @@ def classify_shadows(rgb_shadows, df_shadows, weights_path):
         label, score = get_prediction_score(new_size, model)
         classes.append(label)
         confidence_scores.append(score)
-    print(classes)
+
     df_shadows['class'] = classes
     df_shadows['confidence_score'] = confidence_scores
 
@@ -43,34 +43,3 @@ def get_prediction_score(img_array, model):
     confidence_score = round(100 * np.max(score),3)
 
     return class_label, confidence_score
-
-# def plot_image(i, predictions_array, true_label, img):
-#   true_label, img = true_label[i], img[i]
-#   plt.grid(False)
-#   plt.xticks([])
-#   plt.yticks([])
-#
-#   plt.imshow(img, cmap=plt.cm.binary)
-#
-#   predicted_label = np.argmax(predictions_array)
-#   if predicted_label == true_label:
-#     color = 'blue'
-#   else:
-#     color = 'red'
-#
-#   plt.xlabel("{} {:2.0f}% ({})".format(class_names[predicted_label],
-#                                 100*np.max(predictions_array),
-#                                 class_names[true_label]),
-#                                 color=color)
-#
-# def plot_value_array(i, predictions_array, true_label):
-#   true_label = true_label[i]
-#   plt.grid(False)
-#   plt.xticks(range(10))
-#   plt.yticks([])
-#   thisplot = plt.bar(range(10), predictions_array, color="#777777")
-#   plt.ylim([0, 1])
-#   predicted_label = np.argmax(predictions_array)
-#
-#   thisplot[predicted_label].set_color('red')
-#   thisplot[true_label].set_color('blue')
